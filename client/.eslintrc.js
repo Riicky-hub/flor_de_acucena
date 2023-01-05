55fb42e404b86f525f202fd90eb0b7ede9b880fb
Riicky-hub <richardjosephace1960@gmail.com>
2023-01-05 19:09:31 -0300

    base do projeto e especificacoes/pacotes instalados

---
+++ client/.eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    babelOptions: {
+      parserOpts: {
+        plugins: ['jsx'],
+      },
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 0,
+    'jsx-quotes': ['error', 'prefer-single'],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        jsxSingleQuote: true,
+        parser: 'flow',
+      },
+    ],
+  },
+};
