04fd45d0619eb33e16dd6a7dc0dba2399411dd76
Riicky-hub <richardjosephace1960@gmail.com>
2023-01-10 22:24:54 -0300

    footer finished

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+  Div,
+  Icon,
+  ContainerFooter,
+  Line,
+  Text,
+  ButtonsDiv,
+  FooterDiv,
+} from './styles';
+import { Button } from '../../globalStyles';
+import { instagram, twitter, youtube } from '../../assets/icons';
+import line from '../../assets/bg-frames/lineFrame.svg';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <ContainerFooter>
+        <Div>
+          <Link>
+            <Icon src={twitter} />
+          </Link>
+          <Link>
+            <Icon src={instagram} />
+          </Link>
+          <Link>
+            <Icon src={youtube} />
+          </Link>
+        </Div>
+        <Line src={line} />
+        <Div>
+          <Text>support@openui.design</Text>
+          <Text>+60 825 876</Text>
+          <Text>08:00 - 22:00 - Everyday</Text>
+        </Div>
+        <Line src={line} />
+        <ButtonsDiv>
+          <Button transparent>About</Button>
+          <Button transparent>Contact</Button>
+          <Button transparent>Blog</Button>
+        </ButtonsDiv>
+      </ContainerFooter>
+      <FooterDiv>
+        <Text>Copyright&copy; OpenUI All Rights Reserved.</Text>
+      </FooterDiv>
+    </>
+  );
 };
 
 export default Footer;
---
+++ client/src/components/Footer/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+import { Container, Paragraph } from '../../globalStyles';
+import { InputBackground } from '../../constants/colors';
+
+export const ContainerFooter = styled(Container)`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 5px;
+`;
+
+export const Div = styled.div`
+  width: 100%;
+  padding: 25px 0;
+`;
+
+export const Icon = styled.img`
+  width: 32px;
+  margin: 0 27px;
+`;
+
+export const Line = styled.img`
+  width: 150px;
+`;
+
+export const Text = styled(Paragraph)`
+  color: black;
+  letter-spacing: 2px;
+`;
+
+export const ButtonsDiv = styled(Div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FooterDiv = styled(Div)`
+  background: ${InputBackground};
+  padding: 15px 0;
+  text-align: center;
+`;
